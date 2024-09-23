927f188d644917b477fb400a63393ce794307dfa
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-09-22 20:57:10 -0700

    selesai desain detail food bagian  top

--- app/(dashboard)/(detailFood)/[id].tsx
+++ app/(dashboard)/(detailFood)/[id].tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 // import component
 import { SafeAreaView,ScrollView,StatusBar,Pressable,ImageBackground } from 'react-native'
-import { View,Text,styled, } from 'tamagui'
+import { View,Text,styled, Image, } from 'tamagui'
 import { Ionicons } from '@expo/vector-icons'
 import { Link } from 'expo-router'
 const PressableStyled = styled(Pressable)
@@ -14,9 +14,9 @@ function PageDetailFood() {
   return (
     <SafeAreaView style={{flex:1,position:'relative',backgroundColor:'white'}}>
             <ScrollView style={{flex:1}} contentContainerStyle={{flexGrow:1}}>
-                <View position='relative' w={'100%'} height={'100%'} bg={'$orange10Light'}>
-                  {/*//! header */}
-                  <View position='relative' w={'100%'} pt={StatusBar.currentHeight}  pb={20} bg={'#dddd'}>
+                <View position='relative' w={'100%'} height={'100%'}>
+                  {/*//! TOP */}
+                  <View position='relative' w={'100%'} pt={StatusBar.currentHeight}   bg={'#f79d65'}>
                     {/* header navigasi */}
                     <View position='relative'  w={'100%'} p={5}  flexDirection='row' px={15} justifyContent='space-between' alignItems='center'>
                       {/* icom back */}
@@ -33,8 +33,11 @@ function PageDetailFood() {
                       </PressableStyled>
                     </View>
 
+                    {/* body CONTENT */}
+                    <View position='relative' w={'100%'} px={15} pt={10} pb={20}  justifyContent='center' alignItems='center'>
+                          <Image source={require('@/assets/images/apple.png')}  w={200} h={200} objectFit='contain'/>
+                    </View>
                   </View>
-                  {/*  */}
                 </View>
             </ScrollView>
     </SafeAreaView>
